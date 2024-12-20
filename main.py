5e57d1898f08e45854e09d1380acd0084cc07b2d
gautier-bouetel <gautier.bouetel@edu.esiee.fr>
2024-12-20 12:34:05 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #### Imports et définition des variables globales
-import random
+"""
+Code permettant de lire des données
+"""
 
 FILENAME = "listes.csv"
 
@@ -14,39 +16,57 @@ def read_data(filename):
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
-    return l
+    with open(filename, mode='r', encoding= 'utf8')as f :
+        return [list(map(int, line.strip().split(';'))) for line in f.readlines()]
+
 
 def get_list_k(data, k):
-    l = []
-    return l
-
+    """
+    fonction qui retourne la k-ième liste de donnée
+    """
+    return data[k]
 def get_first(l):
-    return None
+    """
+    Fonction qui retourne le 1er élément d'une liste
+    """
+    return l[0] if l else None
 
 def get_last(l):
-    return None
+    """
+    Fonction qui retourne le dernier élément d'une liste
+    """
+    return l[-1] if l else None
 
 def get_max(l):
-    return None
+    """
+    Fonction qui retourne le max élément d'une liste
+    """
+    return max(l) if l else None
 
 def get_min(l):
-    return None
+    """
+    Fonction qui retourne le min élément d'une liste
+    """
+    return min(l) if l else None
 
 def get_sum(l):
-    return None
+    """
+    Fonction qui retourne la somme des éléments d'une liste
+    """
+    return sum(l) if l else None
 
 
 #### Fonction principale
 
 
 def main():
-    pass
-    # data = read_data(FILENAME)
+    """ fonction principale qui test les fonctions secondaires
+    """
+   # "" data = read_data(FILENAME)
     # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    #    print(i, l)
+    #k = 37
+    #print(k, get_list_k(data, 37))
 
 
 if __name__ == "__main__":
